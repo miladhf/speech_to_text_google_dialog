87c80fe421d7815c86849e7575f2341bb8fe17ed
miladhf <miladhasel@gmail.com>
2023-08-01 18:13:53 +0330

    SpeechToTextGoogle class implemented to use android native code

--- lib/speech_to_text_google.dart
+++ lib/speech_to_text_google.dart
@@ -1,8 +1,29 @@
+library speech_to_text_google;
 
-import 'speech_to_text_google_platform_interface.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
 
 class SpeechToTextGoogle {
-  Future<String?> getPlatformVersion() {
-    return SpeechToTextGooglePlatform.instance.getPlatformVersion();
+  static SpeechToTextGoogle? _instance;
+  final EventChannel _stream = const EventChannel('SpeechToTextGoogleStream');
+  final _platform = const MethodChannel('SpeechToTextGoogleChannel');
+  StreamSubscription? _listener;
+
+  static SpeechToTextGoogle getInstance() {
+    _instance ??= SpeechToTextGoogle();
+    return _instance!;
+  }
+
+  Future<bool> showGoogleDialog(
+      {required Function(dynamic data) onTextReceived}) async {
+    try {
+      final bool? result =
+          await _platform.invokeMethod('showSpeechToTextDialog');
+      _listener ??= _stream.receiveBroadcastStream().listen(onTextReceived);
+      return result ?? false;
+    } on PlatformException catch (e) {
+      return false;
+    }
   }
 }
