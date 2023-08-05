ffb83a3283fc4bf05a3ba795b5ce74de13f865cf
miladhf <miladhasel@gmail.com>
2023-08-05 18:57:34 +0330

    code documentation improved

--- lib/speech_to_text_google_dialog.dart
+++ lib/speech_to_text_google_dialog.dart
@@ -4,23 +4,28 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+///Provides ability to show the Google speech to text dialog
 class SpeechToTextGoogleDialog {
   static SpeechToTextGoogleDialog? _instance;
   final EventChannel _stream = const EventChannel('SpeechToTextGoogleStream');
   final _platform = const MethodChannel('SpeechToTextGoogleChannel');
-  StreamSubscription? _listener;
 
+  ///This function provide singleton object of [SpeechToTextGoogleDialog]
   static SpeechToTextGoogleDialog getInstance() {
     _instance ??= SpeechToTextGoogleDialog();
     return _instance!;
   }
 
+  ///This function showing the Google speech to text dialog
+  ///This function returns a boolean value that indicates service availability
   Future<bool> showGoogleDialog(
       {required Function(dynamic data) onTextReceived}) async {
     try {
+      //call platform specific function
       final bool? result =
           await _platform.invokeMethod('showSpeechToTextDialog');
-      _listener ??= _stream.receiveBroadcastStream().listen(onTextReceived);
+      //passing the received text to [onTextReceived] function
+      _stream.receiveBroadcastStream().listen(onTextReceived);
       return result ?? false;
     } on PlatformException {
       return false;
